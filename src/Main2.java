2c4854996bd5d087f2f96f5ce9f1c659ef8c7a5e
Thimachal <thimachal@gmail.com>
2023-07-07 18:34:02 -0300

    conclusao parte 2

--- src/Main2.java
+++ src/Main2.java
@@ -1,35 +1,38 @@
 //parte 2
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String[] args) {
-        String name = "Joao-m,Marcela-f,Isabela-f,Thiago-m,Neuza-f";
-        String [] resultName = name.split(",");
-        String [] male = new String[30];
-        String [] female = new String[30];;
-        int cont1=0,cont2=0;
+        Scanner scanner= new Scanner(System.in);
+        //String name = "Joao-m,Marcela-f,Isabela-f,Thiago-m,Neuza-f";
+        String name;
+        System.out.println("Digite o nome da pessoa o genero");
+        System.out.println("m se for masculino e f se for feminino");
+        System.out.println("seguido de traço - e após isso digitar virgula ,");
+        System.out.println("Exemplo: thiago-m,maria-f");
 
+        name=scanner.next();
+
+        String [] resultName = name.split(",");
+
+        List<String> male = new ArrayList<String>();
+        List<String> female = new ArrayList<String>();
 
         Arrays.sort(resultName);
-        for (String names: resultName ) {
-            System.out.println(names);
-        }
+
         for (String names: resultName) {
-            if (names.contains("-m")) {
-                male[cont1] = names;
-                cont1++;
+            if(names.contains("-m")){
+                male.add(names);
             } else if (names.contains("-f")) {
-                female[cont2] = names;
+                female.add(names);
             }
         }
-        System.out.print("Grupo Masculino: ");
-        for (String newNames: male) {
-            System.out.print(newNames + ", ");
-        }
-        System.out.println();
-        System.out.print("Grupo Feminino: ");
-        for (String newNames: female) {
-            System.out.print(newNames + ", ");
-        }
+
+        System.out.println("Pessoas do gênero masculino:" + male);
+        System.out.println("Pessoas do gÊnero masculino:" + female);
 
     }
 }
